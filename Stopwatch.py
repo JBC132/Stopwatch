3d70eafb4ec53860bde67a79f13e50b35f6119c5
Jake Rhee <jakerhee321@gmail.com>
2022-12-19 14:20:01 +0900

    Cosmetics2

--- Stopwatch.py
+++ Stopwatch.py
@@ -7,18 +7,22 @@ layout = [
     [sg.Text('time', font='Young 50', key='-TIME-')],
     [
         sg.Button('Start', button_color = ('#FFFFFF', '#FF0000'), border_width=0, key='-STARTSTOP-'), 
-        sg.Button('Lap', button_color = ('#FFFFFF', '#FF0000'), border_width=0, key='-LAP-')]
+        sg.Button('Lap', button_color = ('#FFFFFF', '#FF0000'), border_width=0, key='-LAP-')
+    ],
+    [sg.VPush()]
+
 ]
 
 window = sg.Window(
     'Stopwatch', 
     layout,
     size = (300,300),
-    no_titlebar = True)
+    no_titlebar = True,
+    element_justification = 'center')
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED:
+    if event in (sg.WIN_CLOSED, '-CLOSE-'):
         break
 
 window.close()

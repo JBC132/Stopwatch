71f4517f13dbef2bc390269299a617ade31092ec
Jake Rhee <jakerhee321@gmail.com>
2022-12-22 12:06:17 +0900

    Stop Button

--- Stopwatch.py
+++ Stopwatch.py
@@ -8,7 +8,7 @@ layout = [
     [sg.Text('time', font='Young 50', key='-TIME-')],
     [
         sg.Button('Start', button_color = ('#FFFFFF', '#FF0000'), border_width=0, key='-STARTSTOP-'), 
-        sg.Button('Lap', button_color = ('#FFFFFF', '#FF0000'), border_width=0, key='-LAP-')
+        sg.Button('Lap', button_color = ('#FFFFFF', '#FF0000'), border_width=0, key='-LAP-', visible = False)
     ],
     [sg.VPush()]
 
@@ -24,14 +24,21 @@ start_time = 0
 active = False
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout = 10)
     if event in (sg.WIN_CLOSED, '-CLOSE-'):
         break
 
     if event == '-STARTSTOP-':
-        start_time = time()
-        active = True
-    
+        if active:
+            # active --> stop
+            pass
+        else:
+            # start --> active                
+            start_time = time()
+            active = True
+            window['-STARTSTOP-'].update('Stop')
+            window['-LAP-'].update(visible = True)
+        
     if active:
         elapsed_time = round(time() - start_time,1)
         window['-TIME-'].update(elapsed_time)
